67480f331cbc7b9d4fb927195ee3cebddfdb8bf1
Henry Amos <amoshenry95@gmail.com>
2024-03-04 23:57:59 +0000

    framer motion mobile view resolved

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,17 @@
 import React, { useState } from 'react';
-import { motion, AnimatePresence } from 'framer-motion'; // Import motion components
-import Logo from '/assets/DelLogo.png';
+import { motion, AnimatePresence } from 'framer-motion';
+import Logo from '../../public/assets/DelLogo.png';
 import { NavLink } from 'react-router-dom';
 import { LinkData } from './data/data';
-import { HiOutlineMenuAlt1, HiX } from 'react-icons/hi'; // Import HiX for close icon
+import { HiOutlineMenuAlt1, HiX } from 'react-icons/hi';
 
 const Header = () => {
   const [open, setOpen] = useState(false);
 
-  // Define animation variants
   const variants = {
     open: { opacity: 1, x: 0, transition: { duration: 0.3 } }, // Increase speed by setting duration to 0.3 seconds
     closed: { opacity: 0, x: "-100%", transition: { duration: 0.3 } } // Increase speed by setting duration to 0.3 seconds
-  };
+  }
 
   return (
     <header className='bg-white p-2 z-50 text-black w-full  sticky shadow-md'>
@@ -23,6 +22,23 @@ const Header = () => {
           <img className='h-10' src={Logo} alt="Delkom's Logo" />
          </div>
          {/* Nav Links */}
+         <nav className='desktop-view'> {/* Render desktop view without animation */}
+           <ul className='flex items-center gap-6'>
+             {LinkData.map((link) => (
+               <li className='font-nunito' key={link.id}>
+                 <NavLink
+                   className={({ isActive }) => (isActive ? 'text-darkYellow text-sm  md:text-blue' : 'text-secondary hover:text-darkYellow  md:text-dark')}
+                   to={link.url}
+                 >
+                   {link.title}
+                 </NavLink>
+               </li>
+             ))}
+             <button className='btn-primary font-nunito text-sm '>Join Us</button>
+           </ul>
+         </nav>
+         
+         {/* Mobile View with Framer Motion animation */}
          <AnimatePresence>
            {open && (
              <motion.nav
