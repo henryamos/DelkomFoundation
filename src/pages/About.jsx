7964acf1e4b645502fd740d5ddc4970126b9e279
Henry Amos <amoshenry95@gmail.com>
2024-03-21 02:47:40 +0000

    About Update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,7 +10,8 @@ const About = () => {
 
   return (
     <>
-      <section className=' w-full mx-auto px-0.2 mt-16'>
+      {/* Mobile Section */}
+      <section className='  mx-auto px-0.2 mt-16 md:hidden w-full'>
         <div className='mt-8'>
           <h2 className='text-center text-nunito font-bold text-2xl'>About Us</h2>
         </div>
@@ -34,6 +35,20 @@ const About = () => {
           </p>
         </div>
       </section>
+      {/* Desktop Section */}
+      <section className='w-full mt-16 lg:grid lg:grid-cols-1 xl:grid-cols-12 gap-6'>
+  <div className='xl:col-span-5 lg:min-h-[400px] flex justify-center items-center relative'>
+    <img className='max-w-full h-auto p-1 rounded-xl ' src={backgroundImg1} alt="Delkom Charity Foundation" />
+  </div>
+  <div className='xl:col-span-7 lg:min-h-[400px] flex flex-col justify-center'>
+    <h2 className='text-center text-nunito font-bold text-4xl'>About Us</h2>
+    <p className='text-ptSans mt-4 text-2xl'>
+      Delkom Charity Foundation is a duly registered non-governmental organization based in Ghana, with the aim of fostering a brighter future for individuals through collaborative efforts to empower and assist young, energetic children residing in rural communities. Our targeted age group ranges from 6 to 16 years old.
+      At the core of our programs, we prioritize various initiatives designed to make a positive impact in the lives of these children. This includes the provision of essential school supplies, comprehensive ICT and digital training, opportunities for creative writing and crafting, empowerment activities, Mentorship programs, and coaching sessions.
+    </p>
+  </div>
+</section>
+   
     </>
   );
 };
