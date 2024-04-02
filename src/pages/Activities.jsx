c66d929f61e991a2ea6af107e34b4514f70b3412
Henry Amos <amoshenry95@gmail.com>
2024-04-02 01:27:47 +0000

    Border Update

--- src/pages/Activities.jsx
+++ src/pages/Activities.jsx
@@ -3,11 +3,11 @@ import { actvitiesData } from '../components/data/data';
 
 const Activities = () => {
   return (
-    <section className='mx-auto px-4 mt-16  w-full'>
+    <section className='mx-auto px-2 mt-16  w-full'>
       <h2 className='text-center text-nunito font-bold text-3xl mb-8'>Our Activities</h2>
        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 pt-4 text-ptSans'>
       {actvitiesData.map((activity)=>(
-         <div className='border shadow-xl rounded-lg pt-6 hover:scale-95 duration-300' key={activity.id}>
+         <div className=' shadow-xl rounded-lg pt-6 hover:scale-95 duration-300' key={activity.id}>
             <img className='w-full h-[300px]  object-cover rounded-t-md' src={activity.imgUrl} alt="" />
             <div className='flex flex-col px-2 py-4 min-h-[200px]'>
             <h3 className='font-bold text-xl'>{activity.title}</h3>
