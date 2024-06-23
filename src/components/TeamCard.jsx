721faba174c117b8d5de61c5bec02cfcea6c7131
Henry Amos <amoshenry95@gmail.com>
2024-06-23 20:58:41 +0000

    (Updated Team Card)

--- src/components/TeamCard.jsx
+++ src/components/TeamCard.jsx
@@ -1,9 +1,7 @@
 import React, { useState } from 'react';
 import { teamData } from './data/data'; // Import your data
-import linkedinIcon from '../assets/linkedinSvg.svg';  // Update with the correct path
-import instagramIcon from '../assets/instagram.svg';  // Update with the correct path
-import twitterIcon from '../assets/twitterSvg.svg';  // Update with the correct path
-import useWindowWidth from '../hook/useWindowWidth'  
+import { FaLinkedin, FaInstagram, FaTwitter } from 'react-icons/fa'; // Import icons from react-icons
+import useWindowWidth from '../hook/useWindowWidth';
 
 const TeamCard = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -30,11 +28,11 @@ const TeamCard = () => {
   };
 
   return (
-    <div className="relative w-full bg-dark bg-opacity-90 mx-auto py-4">
+    <div className="relative w-full bg-dark bg-opacity-90 mx-auto py-8">
       <h2 className="text-2xl font-bold text-center text-white md:text-3xl mb-8">
         Meet Our <span className="text-darkYellow">Team</span>
       </h2>
-      <div className="overflow-hidden relative mb-10"> {/* Added margin-bottom to create space for indicators */}
+      <div className="overflow-hidden relative mb-8"> {/* Added margin-bottom to create space for indicators */}
         <div
           className="flex transition-transform duration-500"
           style={{ transform: calculateTransform() }}
@@ -42,7 +40,7 @@ const TeamCard = () => {
           {teamData.map((member) => (
             <div
               key={member.id}
-              className="w-full flex-shrink-0 lg:w-1/3 p-6 md:p-12"
+              className="w-full flex-shrink-0 lg:w-1/3 p-12"
             >
               <div className="w-full lg:max-w-md rounded-lg overflow-hidden shadow-lg bg-dark bg-opacity-90">
                 <img
@@ -53,16 +51,16 @@ const TeamCard = () => {
                 <div className="p-4">
                   <h3 className="text-xl font-bold text-white">{member.name}</h3>
                   <div className="flex justify-between items-center mt-2">
-                    <p className="text-darkYellow">{member.role}</p>
-                    <div className="flex space-x-2 mr-4">
-                      <a href={member.socialLinks.linkedin} target="_blank" rel="noopener noreferrer">
-                        <img src={linkedinIcon} alt="LinkedIn" className="w-8 h-8" />
+                    <p className="font-bold text-darkYellow">{member.role}</p>
+                    <div className="flex space-x-2">
+                      <a href={member.socialLinks.linkedin} target="_blank" rel="noopener noreferrer" className="bg-gray-800 p-2 rounded-full flex items-center justify-center hover:bg-darkYellow hover:text-dark transition-colors duration-300">
+                        <FaLinkedin className="text-primary hover:text-dark w-4 h-4" />
                       </a>
-                      <a href={member.socialLinks.instagram} target="_blank" rel="noopener noreferrer">
-                        <img src={instagramIcon} alt="Instagram" className="w-8 h-8" />
+                      <a href={member.socialLinks.instagram} target="_blank" rel="noopener noreferrer" className="bg-gray-800 p-2 rounded-full flex items-center justify-center hover:bg-darkYellow hover:text-dark transition-colors duration-300">
+                        <FaInstagram className="text-primary hover:text-dark w-4 h-4" />
                       </a>
-                      <a href={member.socialLinks.twitter} target="_blank" rel="noopener noreferrer">
-                        <img src={twitterIcon} alt="Twitter" className="w-8 h-8" />
+                      <a href={member.socialLinks.twitter} target="_blank" rel="noopener noreferrer" className="bg-gray-800 p-2 rounded-full flex items-center justify-center hover:bg-darkYellow hover:text-dark transition-colors duration-300">
+                        <FaTwitter className="text-primary hover:text-dark w-4 h-4" />
                       </a>
                     </div>
                   </div>
@@ -73,22 +71,22 @@ const TeamCard = () => {
         </div>
         <button
           onClick={handlePrev}
-          className="absolute top-1/2 left-1 transform -translate-y-1/2 p-2 bg-darkYellow rounded-full"
+          className="absolute top-1/2 left-2 transform -translate-y-1/2 p-2 bg-darkYellow rounded-full"
         >
           &lt;
         </button>
         <button
           onClick={handleNext}
-          className="absolute top-1/2 right-0 transform -translate-y-1/2 p-2 bg-darkYellow rounded-full"
+          className="absolute top-1/2 right-2 md:right-6 transform -translate-y-1/2 p-2 bg-darkYellow rounded-full"
         >
           &gt;
         </button>
-        <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 flex space-x-4 mt-4">
+        <div className="absolute -bottom-0 left-1/2 transform -translate-x-1/2 flex space-x-8 mt-4">
           {(isLargeScreen ? Array(3).fill(0) : teamData).map((_, index) => (
             <div
               key={index}
-              className={`h-2 w-2  rounded-full ${
-                index === currentIndex % (isLargeScreen ? 3 : teamData.length) ? 'bg-darkYellow' : 'bg-gray-500'
+              className={`h-2 w-2 rounded-full ${
+                index === currentIndex % (isLargeScreen ? 3 : teamData.length) ? 'bg-darkYellow' : 'bg-gray-100'
               }`}
             />
           ))}
