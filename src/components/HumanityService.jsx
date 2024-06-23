16be688d179a0949ce91b2e7d66d9e00a45bda63
Henry Amos <amoshenry95@gmail.com>
2024-06-23 14:53:23 +0000

    (Service Page Improve)

--- src/components/HumanityService.jsx
+++ src/components/HumanityService.jsx
@@ -1,39 +1,54 @@
-import React from 'react'
+import React from 'react';
 import backgroundImg1 from '/assets/Delkom9.jpg';
 import booking from '../assets/map_school.svg';
 import clothing from '../assets/game-icons_clothes.svg';
-import food from '../assets/fluent_food-24-filled.svg'
+import food from '../assets/fluent_food-24-filled.svg';
+
 const HumanityServiceCard = () => {
   return (
-    <div className="w-full min-h-screen mx-auto bg-bgDark ">
-
-      <div className='relative min-h-screen'>
-        <h2 className='text-center text-primary'>Our <span className='text-darkYellow'>Humanity Service</span> </h2>
-        <div className='absolute flex flex-col items-center justify-center w-full min-h-screen font-nunito text-primaryBase bg-dark/80 md:flex-row md:justify-around'>
-            
-            {/* Food Icons */}
-            <div className='flex flex-col items-center justify-center w-1/2'>
-            <img  className='w-12 md:w-24' src={food} alt="" />
-            <h4 className='mt-4 mb-4 text-lg font-normal md:font-bold md:text-2xl'>Charity For Food</h4>
-            <p className='text-center'>Our members volunteer to support and  <br className='hidden md:block' /> educate young children residing in rural areas</p>
-            </div>
-              {/*  Icons */}
-            <div className='flex flex-col items-center justify-center w-1/2'>
-            <img className='w-12 md:w-24' src={clothing} alt="" />
-            <h3 className='mt-4 mb-4 text-lg font-normal md:font-bold md:text-2xl'>Charity For Clothes</h3>
-            <p className='text-center'>Our members volunteer to support and <br />  educate young children residing in rural areas</p>
-           </div>
-           <div className='flex flex-col items-center justify-center w-1/2'>
-            <img className='w-12 md:w-24' src={booking} alt="" />
-            <h3 className='mt-4 mb-4 font-normal md:font-bold text-md md:text-2xl'>Charity For Education</h3>
-            <p className='text-center'>Our members volunteer to support and <br />  educate young children residing in rural areas</p>
-            </div>
-           
+    <div className="relative w-full h-screen md:h-[50vh] bg-bgDark">
+      <img className="absolute inset-0 object-cover w-full h-full" src={backgroundImg1} alt="Background" />
+      <div className="relative z-10 flex flex-col items-center justify-center h-full bg-black bg-opacity-70">
+        <h2 className="text-2xl font-bold text-center text-white md:text-3xl mb-4 md:mb-8">
+          Our <span className="text-darkYellow">Services To Humanity</span>
+        </h2>
+        <div className="flex flex-col items-center justify-center w-full max-w-4xl mx-auto space-y-6 px-4 md:flex-row md:space-y-0
+       
+        md:justify-between lg:space-x-24">
+          {/* Charity for Food */}
+          <div className="flex flex-col items-center justify-center w-full md:w-1/2">
+            <img className="w-10 md:w-14" src={food} alt="Charity For Food" />
+            <h4 className="mt-2 mb-1 text-lg font-normal text-center text-white md:font-bold md:text-xl">
+              Charity For Food
+            </h4>
+            <p className="text-center text-white text-sm md:text-base">
+              Our members volunteer to support and educate young children residing in rural areas
+            </p>
+          </div>
+          {/* Charity for Clothes */}
+          <div className="flex flex-col items-center justify-center w-full md:w-1/2">
+            <img className="w-10 md:w-14" src={clothing} alt="Charity For Clothes" />
+            <h4 className="mt-2 mb-1 text-lg font-normal text-center text-white md:font-bold md:text-xl">
+              Charity For Clothes
+            </h4>
+            <p className="text-center text-white text-sm md:text-base">
+              Our members volunteer to support and educate young children residing in rural areas
+            </p>
+          </div>
+          {/* Charity for Education */}
+          <div className="flex flex-col items-center justify-center w-full md:w-1/2">
+            <img className="w-10 md:w-14" src={booking} alt="Charity For Education" />
+            <h4 className="mt-2 mb-1 text-lg font-normal text-center text-white md:font-bold md:text-xl">
+              Charity For Education
+            </h4>
+            <p className="text-center text-white text-sm md:text-base">
+              Our members volunteer to support and educate young children residing in rural areas
+            </p>
+          </div>
         </div>
-        <img className='object-cover w-full min-h-screen' src={backgroundImg1} alt="" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default HumanityServiceCard
+export default HumanityServiceCard;
--- src/pages/OurImpact.jsx
+++ src/pages/OurImpact.jsx
@@ -3,7 +3,7 @@ import HumanityServiceCard from '../components/HumanityService';
 
 const OurImpact = () => {
   return (
-    <section className='px-4 mx-auto mt-16 '>
+    <section className='px-4 mx-auto mt-10  '>
 
       <HumanityServiceCard/>
     </section>
