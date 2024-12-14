0a916c38db2cbf3b4bbfdd92093c5db6bbcd7548
Henry Amos <amoshenry95@gmail.com>
2024-12-14 13:44:39 +0000

    💄 style(footer): update footer and improve mobile responsiveness ✨ feat(footer): update footer for mobile responsiveness 📱 refactor(ui): update footer and improve mobile responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ function Footer() {
     <footer className="bg-black text-white py-8">
       <div className="max-w-6xl mx-auto px-4 lg:px-2">
         <div className="flex flex-col lg:space-x-16 md:flex-row justify-between items-start">
-          <div className="flex flex-col ml-4  items-start mb-4 md:mb-0 md:w-1/3">
+          <div className="flex flex-col  lg:ml-4  items-start mb-4 md:mb-0 md:w-1/3">
             <h2 className="headings font-bold mb-2 text-white hover:text-darkYellow">
               Delkom Charity Foundation
             </h2>
@@ -20,81 +20,83 @@ function Footer() {
             </p>
           </div>
           <div className="flex flex-col md:flex-row md:space-x-16 items-start md:w-2/3 mt-4 md:mt-0">
-            <div className="flex flex-col items-start mb-4 md:mb-0">
-              <h2 className="text-lg font-bold mb-4 text-white hover:text-darkYellow">
-                Pages
-              </h2>
-              <ul className="space-y-2">
-                <li>
-                  <Link to="/" className="text-white hover:text-darkYellow">
-                    Home
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/about" className="text-white hover:text-darkYellow">
-                    About
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/activities" className="text-white hover:text-darkYellow">
-                    Activities
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/our-impact" className="text-white hover:text-darkYellow">
-                    Our Impact
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/team" className="text-white hover:text-darkYellow">
-                    Team
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/contact-us" className="text-white hover:text-darkYellow">
-                    Contact Us
-                  </Link>
-                </li>
-              </ul>
-            </div>
-            <div className="flex flex-col items-start">
-              <h2 className="text-lg font-bold mb-4 text-white hover:text-darkYellow">
-                Connect with Us
-              </h2>
-              <ul className="space-y-2">
-                <li>
-                  <a
-                    href="https://www.facebook.com/DelkomCharityFoundation"
-                    className="flex items-center text-white hover:text-darkYellow"
-                  >
-                    <FaFacebook className="mr-2" /> Facebook
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://twitter.com/DelkomCharity"
-                    className="flex items-center text-white hover:text-darkYellow"
-                  >
-                    <FaTwitter className="mr-2" /> Twitter
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://www.instagram.com/DelkomCharityFoundation"
-                    className="flex items-center text-white hover:text-darkYellow"
-                  >
-                    <FaInstagram className="mr-2" /> Instagram
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="mailto:info@delkomcharity.com"
-                    className="flex items-center text-white hover:text-darkYellow"
-                  >
-                    <FaEnvelope className="mr-2" /> Email
-                  </a>
-                </li>
-              </ul>
+            <div className="grid grid-cols-2 gap-8 w-full">
+              <div className="flex flex-col items-start">
+                <h2 className="text-lg font-bold mb-4 text-white hover:text-darkYellow">
+                  Pages
+                </h2>
+                <ul className="space-y-2">
+                  <li>
+                    <Link to="/" className="text-white hover:text-darkYellow">
+                      Home
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/about" className="text-white hover:text-darkYellow">
+                      About
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/activities" className="text-white hover:text-darkYellow">
+                      Activities
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/our-impact" className="text-white hover:text-darkYellow">
+                      Our Impact
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/team" className="text-white hover:text-darkYellow">
+                      Team
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/contact-us" className="text-white hover:text-darkYellow">
+                      Contact Us
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+              <div className="flex flex-col items-start">
+                <h2 className="text-lg font-bold mb-4 text-white hover:text-darkYellow">
+                  Connect with Us
+                </h2>
+                <ul className="space-y-2">
+                  <li>
+                    <a
+                      href="https://www.facebook.com/DelkomCharityFoundation"
+                      className="flex items-center text-white hover:text-darkYellow"
+                    >
+                      <FaFacebook className="mr-2" /> Facebook
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="https://twitter.com/DelkomCharity"
+                      className="flex items-center text-white hover:text-darkYellow"
+                    >
+                      <FaTwitter className="mr-2" /> Twitter
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="https://www.instagram.com/DelkomCharityFoundation"
+                      className="flex items-center text-white hover:text-darkYellow"
+                    >
+                      <FaInstagram className="mr-2" /> Instagram
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="mailto:info@delkomcharity.com"
+                      className="flex items-center text-white hover:text-darkYellow"
+                    >
+                      <FaEnvelope className="mr-2" /> Email
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
